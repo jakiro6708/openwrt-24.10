36ebffe072bd65500d19fa9a3e7d69c7c1e846de
Bruce6709 <102840341+Bruce6709@users.noreply.github.com>
2025-02-20 15:28:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,9 +25,9 @@ sed -i 's/dnsmasq /dnsmasq-full blockd libopenssl-legacy libopenssl-conf zerotie
 # 带USB的路由器
 sed -i 's/ppp-mod-pppoe/ppp-mod-pppoe vsftpd kmod-fs-exfat kmod-fs-ext4 kmod-fs-vfat kmod-usb-storage kmod-usb2 kmod-usb3 luci-app-hd-idle luci-app-samba4/g' include/target.mk
 
-# chmod +x luciMenu.py
-# python3 luciMenu.py
-# rm -rf luciMenu.py
+chmod +x luciMenu.py
+python3 luciMenu.py
+rm -rf luciMenu.py
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
